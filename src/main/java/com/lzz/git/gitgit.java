a86ca4181df7f60d41462544f5dde9f1a81889f5
lzzNBnb <88418027@qq.com>
2023-07-19 15:39:46 +0800

    third  commit

--- src/main/java/com/lzz/git/gitgit.java
+++ src/main/java/com/lzz/git/gitgit.java
@@ -4,5 +4,6 @@ public class gitgit {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("hello git");
+        System.out.println("hello lzz");
     }
 }
